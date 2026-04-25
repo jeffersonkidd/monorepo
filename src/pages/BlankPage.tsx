fc30b19782636a3559df54968b2bfe64ec0312f1
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-25 14:49:05 -0400

    Create BlankPage.tsx

---
+++ src/pages/BlankPage.tsx
@@ -0,0 +1,23 @@
+import { BlankTemplate } from "templates";
+import { Header, Footer } from "compositions";
+import { TextHeading, Text } from "primitives";
+import { Stack, Grid, Flex, Box, Container } from "layout";
+
+export function BlankPage() {
+  return (
+        <BlankTemplate
+          header={<Header />}
+          footer={<Footer />}
+          id="dashboard-page"
+          mainProps={{ "aria-label": "Dashboard content" }}
+        >
+    <Section padding=“600” variant=“brand”>
+     <Flex direction=“column” alignSecondary=“center”>
+          <TextHeading>Overview</TextHeading>
+          <Text>Welcome to your dashboard.</Text>
+      </Flex>
+      </Center>
+        </BlankTemplate>
+
+  );
+}
