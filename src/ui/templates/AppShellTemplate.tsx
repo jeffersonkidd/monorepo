66047c04d5a2a2fe426a6c244a9129b3546003f4
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-25 18:20:53 -0400

    Update AppShellTemplate.tsx

--- src/ui/templates/AppShellTemplate.tsx
+++ src/ui/templates/AppShellTemplate.tsx
@@ -1,10 +1,7 @@
+// AppShellTemplate.tsx
 import { Sidebar } from "compositions";
 import { Flex, FlexItem, Section } from "layout";
-import {
-  Navigation,
-  NavigationPill,
-  TextHeading,
-} from "primitives";
+import { Navigation, NavigationPill, TextHeading } from "primitives";
 import { ReactNode, useState } from "react";
 import "./templates.css";
 
@@ -42,56 +39,54 @@ export function AppShellTemplate({
   };
 
   return (
-    <div className="template-page-root">
-      <Section variant="neutral" padding="600">
-        <Flex container gap="600" alignSecondary="start">
+    <Section variant="neutral" padding="600">
+      <Flex container gap="600" alignSecondary="start">
 
-          <FlexItem size="minor">
-            <Sidebar brand={brand}>
-              <Navigation direction="column">
-                {navItems.map((item) => (
-                  <NavigationPill
-                    key={item.key}
-                    isSelected={activeKey === item.key}
-                    onPress={() => handleNavChange(item.key)}
-                  >
-                    {item.label}
-                  </NavigationPill>
-                ))}
-              </Navigation>
-            </Sidebar>
-          </FlexItem>
-
-          <FlexItem size="major">
-            <Flex gap="600" alignSecondary="start">
-
-              <FlexItem size="major">
-                <main
-                  className="template-block"
-                  aria-label="App shell primary content"
+        <FlexItem size="minor">
+          <Sidebar brand={brand}>
+            <Navigation direction="column">
+              {navItems.map((item) => (
+                <NavigationPill
+                  key={item.key}
+                  isSelected={activeKey === item.key}
+                  onPress={() => handleNavChange(item.key)}
                 >
-                  <Flex direction="column" gap="600">
-                    {renderMain(activeKey)}
+                  {item.label}
+                </NavigationPill>
+              ))}
+            </Navigation>
+          </Sidebar>
+        </FlexItem>
+
+        <FlexItem size="major">
+          <Flex gap="600" alignSecondary="start">
+
+            <FlexItem size="major">
+              <main
+                className="template-block"
+                aria-label="App shell primary content"
+              >
+                <Flex direction="column" gap="600">
+                  {renderMain(activeKey)}
+                </Flex>
+              </main>
+            </FlexItem>
+
+            {renderAside && (
+              <FlexItem size="minor">
+                <aside className="template-block">
+                  <Flex direction="column" gap="400">
+                    <TextHeading>{asideHeading}</TextHeading>
+                    {renderAside(activeKey)}
                   </Flex>
-                </main>
+                </aside>
               </FlexItem>
+            )}
 
-              {renderAside && (
-                <FlexItem size="minor">
-                  <aside className="template-block">
-                    <Flex direction="column" gap="400">
-                      <TextHeading>{asideHeading}</TextHeading>
-                      {renderAside(activeKey)}
-                    </Flex>
-                  </aside>
-                </FlexItem>
-              )}
+          </Flex>
+        </FlexItem>
 
-            </Flex>
-          </FlexItem>
-
-        </Flex>
-      </Section>
-    </div>
+      </Flex>
+    </Section>
   );
 }
