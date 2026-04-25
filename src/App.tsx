57d508fd66623113559365d4f60b846e0f002310
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-25 14:51:34 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
 import { AllProviders } from "data";
-import { BlankTemplate } from "templates";
-import { Header, Footer } from "compositions";
-import { TextHeading, Text } from "primitives";
+import { BlankPage } from "./pages/BlankPage"
 
 function App() {
   return (
     <AllProviders>
-        <BlankTemplate
-          header={<Header />}
-          footer={<Footer />}
-          id="dashboard-page"
-          mainProps={{ "aria-label": "Dashboard content" }}
-        >
-          <TextHeading>Overview</TextHeading>
-          <Text>Welcome to your dashboard.</Text>
-        </BlankTemplate>
+        <BlankPage>
     </AllProviders>
   );
 }
