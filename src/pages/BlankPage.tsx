8668ddc97e98bc661b56d0162a0328675aac92ba
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-25 14:57:18 -0400

    Update BlankPage.tsx

--- src/pages/BlankPage.tsx
+++ src/pages/BlankPage.tsx
@@ -1,23 +1,22 @@
 import { BlankTemplate } from "templates";
 import { Header, Footer } from "compositions";
 import { TextHeading, Text } from "primitives";
-import { Stack, Grid, Flex, Box, Container } from "layout";
+import { Stack, Grid, Flex, Box, Container, Section } from "layout";
 
 export function BlankPage() {
   return (
-        <BlankTemplate
-          header={<Header />}
-          footer={<Footer />}
-          id="dashboard-page"
-          mainProps={{ "aria-label": "Dashboard content" }}
-        >
-    <Section padding=“600” variant=“brand”>
-     <Flex direction=“column” alignSecondary=“center”>
+    <BlankTemplate
+      header={<Header />}
+      footer={<Footer />}
+      id="dashboard-page"
+      mainProps={{ "aria-label": "Dashboard content" }}
+    >
+      <Section padding="600" variant="brand">
+        <Flex direction="column" alignSecondary="center">
           <TextHeading>Overview</TextHeading>
           <Text>Welcome to your dashboard.</Text>
-      </Flex>
+        </Flex>
       </Section>
-        </BlankTemplate>
-
+    </BlankTemplate>
   );
 }
