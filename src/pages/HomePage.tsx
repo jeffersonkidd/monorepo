f0f0e3475470e78b42d4ca42c465f4c79f2f199d
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-26 01:00:56 -0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,10 @@
-import { WelcomeHero } from "examples/WelcomeHero";
-import { PanelSections } from "examples/PanelSections";
+import { WelcomeHero } from "../examples/WelcomeHero";
+import { PanelSections } from "../examples/PanelSections";
 import { Demo } from "../examples/Demo";
-import { Header, Footer } from "compositions";
-import { ProductDetails } from "examples/ProductDetails"; 
-import { PricingGrid } from "examples/PricingGrid";
-import { ProductGrid } from "examples/ProductGrid";
+import { Header, Footer } from "../compositions";
+import { ProductDetails } from "../examples/ProductDetails"; 
+import { PricingGrid } from "../examples/PricingGrid";
+import { ProductGrid } from "../examples/ProductGrid";
 
 export function HomePage() {
   return (
