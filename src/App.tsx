7216b83bc022e51f871408dfec2d76f740267c9f
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-26 04:53:23 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { AllProviders } from "data";
-import { LinksPage } from "pages/LinksPage3";
+import { MarketingTemplate } from "templates";
 
 function App() {
   return (
     <AllProviders>
-      <LinksPage />
+      <MarketingTemplate />
     </AllProviders>
   );
 }
