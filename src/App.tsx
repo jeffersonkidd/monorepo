0c7441ede2effe94193f7cd659821a3c0eb3ac81
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-26 03:17:38 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { AllProviders } from "data";
-import { AppShellTemplate } from "templates";
+import { OverviewPage } from "pages/OverviewPage.tsx";
 
 function App() {
   return (
     <AllProviders>
-      <AppShellTemplate />
+      <OverviewPage />
     </AllProviders>
   );
 }
