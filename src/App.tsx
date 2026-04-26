4e3030b24bb1e23a1b83f4c6e5bc26c678e343dc
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-25 20:49:17 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { AllProviders } from "data";
-import { AppPage } from "./pages/WorkspacePage"
+import { WorkspacePage } from "./pages/WorkspacePage"
 
 function App() {
   return (
     <AllProviders>
-        <AppPage />
+        <WorkspacePage />
     </AllProviders>
   );
 }
