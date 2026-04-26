c13488272142f073551300b1254405b18b2c9223
Jefferson Kidd <jeffersonkidd@hotmail.com>
2026-04-26 05:17:54 -0400

    Update BlankPage.tsx

--- src/pages/BlankPage.tsx
+++ src/pages/BlankPage.tsx
@@ -1,7 +1,5 @@
 import { BlankTemplate } from "templates";
 import { Header, Footer } from "compositions";
-import { TextHeading, Text } from "primitives";
-import { Flex,Section } from "layout";
 import { WelcomeHero } from "examples/WelcomeHero";
 
 export function BlankPage() {
